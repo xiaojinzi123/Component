a4c4e6dd4f4d027a8f5a2a3f5791ebbbf721cc5b
xiaojinzi <347837667@qq.com>
2019-01-21 18:25:49 +0800

    能单独运行业务模块的 groovy 代码编写中1

---
+++ Component1/src/main/runalone/java/com/ehi/component1/debug/MainActActivity.java
@@ -0,0 +1,15 @@
+package com.ehi.component1.debug;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ehi.component1.R;
+
+public class MainActActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.component1_debug_main_act);
+    }
+}
