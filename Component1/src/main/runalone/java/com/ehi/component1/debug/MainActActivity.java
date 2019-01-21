1ea52a573af5499e5779b654af030e0e993ba73d
xiaojinzi <347837667@qq.com>
2019-01-21 18:26:30 +0800

    能单独运行业务模块的 groovy 代码编写中1

--- Component1/src/main/runalone/java/com/ehi/component1/debug/MainActActivity.java
+++ Component1/src/main/runalone/java/com/ehi/component1/debug/MainActActivity.java
@@ -1,7 +1,7 @@
 package com.ehi.component1.debug;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.ehi.component1.R;
 
