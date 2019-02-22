adc98170f1cd83949b6e5317b8af82e72ee76457
passin <zengbinbinpassin@foxmail.com>
2019-02-22 12:35:43 +0800

    添加缓存工厂模型和基本模型 LruCache。

---
+++ ComponentImpl/src/main/java/com/ehi/component/cache/CacheType.java
@@ -0,0 +1,54 @@
+package com.ehi.component.cache;
+
+import android.app.ActivityManager;
+import android.content.Context;
+
+/**
+ * 构建 {@link Cache} 时,使用 {@link CacheType} 中声明的类型,来区分不同的模块
+ * 从而为不同的模块构建不同的缓存策略
+ */
+public interface CacheType {
+    int ROUTER_INTERCEPTOR_CACHE_TYPE_ID = 0;
+    int CACHE_SERVICE_CACHE_TYPE_ID = 1;
+    /**
+     * 缓存 {@link com.ehi.component.impl.EHiRouterInterceptor} 的容器
+     */
+    CacheType ROUTER_INTERCEPTOR_CACHE = new CacheType() {
+        private static final int MAX_SIZE = 25;
+
+        @Override
+        public int getCacheTypeId() {
+            return ROUTER_INTERCEPTOR_CACHE_TYPE_ID;
+        }
+
+        @Override
+        public int calculateCacheSize(Context context) {
+            ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+            int targetMemoryCacheSize;
+            if (CacheHelper.isLowMemoryDevice(activityManager)) {
+                targetMemoryCacheSize = activityManager.getMemoryClass() / 6;
+            } else {
+                targetMemoryCacheSize = activityManager.getMemoryClass() / 4;
+            }
+            if (targetMemoryCacheSize > MAX_SIZE) {
+                return MAX_SIZE;
+            }
+            return targetMemoryCacheSize;
+        }
+    };
+
+
+    /**
+     * 返回框架内需要缓存的模块对应的 {@code id}
+     *
+     * @return
+     */
+    int getCacheTypeId();
+
+    /**
+     * 计算对应模块需要的缓存大小
+     *
+     * @return
+     */
+    int calculateCacheSize(Context context);
+}
