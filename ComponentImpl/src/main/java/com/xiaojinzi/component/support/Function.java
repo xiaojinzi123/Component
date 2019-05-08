53ca8fc2665e8ba837142a6500a6f23696ce11d5
xiaojinzi <cxj0xiaojinzi@gmail.com>
2019-05-08 20:32:48 +0800

    Rx 库方法整改

---
+++ ComponentImpl/src/main/java/com/xiaojinzi/component/support/Function.java
@@ -0,0 +1,17 @@
+package com.xiaojinzi.component.support;
+
+import android.support.annotation.NonNull;
+
+public interface Function<T, R> {
+
+    /**
+     * 做一个转化,从一个对象变成另一个对象
+     *
+     * @param t
+     * @return
+     * @throws Exception
+     */
+    @NonNull
+    R apply(@NonNull T t) throws Exception;
+
+}
