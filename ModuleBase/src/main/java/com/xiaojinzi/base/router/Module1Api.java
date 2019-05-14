866412299f0510cd38c0dd46106a61b3bc221855
xiaojinzi <cxj0xiaojinzi@gmail.com>
2019-05-14 18:07:02 +0800

    计划支持 Api 的调用方式

---
+++ ModuleBase/src/main/java/com/xiaojinzi/base/router/Module1Api.java
@@ -0,0 +1,18 @@
+package com.xiaojinzi.base.router;
+
+import com.xiaojinzi.base.ModuleConfig;
+import com.xiaojinzi.component.anno.ParameterAnno;
+import com.xiaojinzi.component.anno.router.HostAnno;
+import com.xiaojinzi.component.anno.router.PathAnno;
+import com.xiaojinzi.component.anno.router.RouterApiAnno;
+
+/**
+ * App 模块的路由跳转接口
+ */
+@RouterApiAnno()
+@HostAnno(ModuleConfig.Module1.NAME)
+public interface Module1Api {
+
+    @PathAnno(ModuleConfig.Module1.TEST)
+    void test(@ParameterAnno("data") String data);
+}
