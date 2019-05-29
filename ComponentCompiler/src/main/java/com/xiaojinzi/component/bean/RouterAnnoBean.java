922fe6f47bad766638ca23c05d14163a52ec6c4a
xiaojinzi <cxj0xiaojinzi@gmail.com>
2019-05-29 20:33:29 +0800

    全面支持 @RouterAnno()的hostAndPath属性

--- ComponentCompiler/src/main/java/com/xiaojinzi/component/bean/RouterAnnoBean.java
+++ ComponentCompiler/src/main/java/com/xiaojinzi/component/bean/RouterAnnoBean.java
@@ -12,6 +12,8 @@ import javax.lang.model.element.Element;
  */
 public class RouterAnnoBean {
 
+    private String host;
+    private String path;
     private String desc;
     // value是实现类的全类名
     private List<String> interceptors = new ArrayList<>(1);
@@ -19,6 +21,22 @@ public class RouterAnnoBean {
     private List<String> interceptorNames = new ArrayList<>(1);
     // 可是是一个Activity 类或者是一个静态方法
     private Element rawType;
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
 
     public String getDesc() {
         return desc;
@@ -43,4 +61,9 @@ public class RouterAnnoBean {
     public List<String> getInterceptorNames() {
         return interceptorNames;
     }
+
+    public String hostAndPath() {
+        return host + path;
+    }
+
 }
