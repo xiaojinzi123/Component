9881e727bf570e6350791f0ebe726d16c7831ea6
xiaojinzi <cxj0xiaojinzi@gmail.com>
2019-05-30 17:56:19 +0800

    nothing

--- ModuleBase/src/main/java/com/xiaojinzi/base/router/AppApi.java
+++ ModuleBase/src/main/java/com/xiaojinzi/base/router/AppApi.java
@@ -2,8 +2,10 @@ package com.xiaojinzi.base.router;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 
 import com.xiaojinzi.base.ModuleConfig;
+import com.xiaojinzi.component.anno.router.FlagAnno;
 import com.xiaojinzi.component.anno.router.HostAnno;
 import com.xiaojinzi.component.anno.router.PathAnno;
 import com.xiaojinzi.component.anno.router.RouterApiAnno;
@@ -13,6 +15,7 @@ import com.xiaojinzi.component.anno.router.RouterApiAnno;
 public interface AppApi {
 
     @PathAnno(ModuleConfig.App.TEST_ROUTER)
+    @FlagAnno(Intent.FLAG_ACTIVITY_NEW_TASK)
     void goToTestRouter(Activity context);
 
     @PathAnno(ModuleConfig.App.TEST_QUALITY)
