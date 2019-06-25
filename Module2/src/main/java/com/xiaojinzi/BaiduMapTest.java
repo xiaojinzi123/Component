162942fba626733e148ed01de6771eb8501991b2
xiaojinzi <cxj0xiaojinzi@gmail.com>
2019-06-25 16:08:53 +0800

    删除清单文件中重复的 host 信息

---
+++ Module2/src/main/java/com/xiaojinzi/BaiduMapTest.java
@@ -0,0 +1,4 @@
+package com.xiaojinzi;
+
+public class BaiduMapTest {
+}
--- ModuleBase/src/main/java/com/xiaojinzi/base/router/AppApi.java
+++ ModuleBase/src/main/java/com/xiaojinzi/base/router/AppApi.java
@@ -2,10 +2,8 @@ package com.xiaojinzi.base.router;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 
 import com.xiaojinzi.base.ModuleConfig;
-import com.xiaojinzi.component.anno.router.FlagAnno;
 import com.xiaojinzi.component.anno.router.HostAnno;
 import com.xiaojinzi.component.anno.router.PathAnno;
 import com.xiaojinzi.component.anno.router.RouterApiAnno;
@@ -17,7 +15,6 @@ import io.reactivex.Completable;
 public interface AppApi {
 
     @PathAnno(ModuleConfig.App.TEST_ROUTER)
-    @FlagAnno(Intent.FLAG_ACTIVITY_NEW_TASK)
     void goToTestRouter(Activity context);
 
     @PathAnno(ModuleConfig.App.TEST_QUALITY)
