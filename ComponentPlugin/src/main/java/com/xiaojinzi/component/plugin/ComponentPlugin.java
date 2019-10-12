2b259fb7b292a71f977667b62851f6d35cddcf91
xiaojinzi <cxj0xiaojinzi@gmail.com>
2019-10-12 17:51:40 +0800

    创建 Plugin 的目录, 准备生成初始化反射的寻找实现类的一些实现

---
+++ ComponentPlugin/src/main/java/com/xiaojinzi/component/plugin/ComponentPlugin.java
@@ -0,0 +1,24 @@
+package com.xiaojinzi.component.plugin;
+
+import com.android.build.gradle.internal.dsl.BaseAppModuleExtension;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+/**
+ * 组件化的 Gradle 插件, 为了生成一部分的代码, 代替反射查找这个过程, 整个流程已经设计好了
+ * 只要把 ASMUtil 工具类中的空方法填写一下就可以了
+ */
+public class ComponentPlugin implements Plugin<Project> {
+
+    @Override
+    public void apply(Project project) {
+        System.out.println("=============oooooooooooooooooo===================================" + project.getProperties().get("android").getClass().getName());
+        BaseAppModuleExtension appModuleExtension =  (BaseAppModuleExtension)project.getProperties().get("android");
+        appModuleExtension.registerTransform(new ComponentTransform());
+        /*AppExtension appExtension = (AppExtension) project.getProperties().get("android");
+        BaseAppModuleExtension
+        appExtension.registerTransform(new ComponentTransform());*/
+    }
+
+}
