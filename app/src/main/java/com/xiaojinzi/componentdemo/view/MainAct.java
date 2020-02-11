7b57d8d245267add179fe9623128154570759dbb
jimGreen <347837667@qq.com>
2020-02-11 21:08:52 +0800

    准备发布 1.7.9-androidx

--- app/src/main/java/com/xiaojinzi/componentdemo/view/MainAct.java
+++ app/src/main/java/com/xiaojinzi/componentdemo/view/MainAct.java
@@ -5,7 +5,6 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
