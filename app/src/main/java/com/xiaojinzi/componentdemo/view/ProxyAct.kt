a8850b58b0784a0ddc4310cf90fc7ca7971e0937
jimGreen <347837667@qq.com>
2020-02-11 21:13:34 +0800

    解决 support 包的问题

--- app/src/main/java/com/xiaojinzi/componentdemo/view/ProxyAct.kt
+++ app/src/main/java/com/xiaojinzi/componentdemo/view/ProxyAct.kt
@@ -1,10 +1,9 @@
 package com.xiaojinzi.componentdemo.view
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.xiaojinzi.componentdemo.R
-import com.xiaojinzi.componentdemo.util.ToastUtil
 
 class ProxyAct : AppCompatActivity() {
 
