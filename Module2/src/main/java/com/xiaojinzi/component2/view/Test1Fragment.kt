4baaf3208f461cd35b7143ebf9fb7156347678c6
xiaojinzi <cxj0xiaojinzi@gmail.com>
2020-07-15 20:11:33 +0800

    增加 kotlin @FragmentAnno 标记在方法上的事例和升级第三方库的版本

--- Module2/src/main/java/com/xiaojinzi/component2/view/Test1Fragment.kt
+++ Module2/src/main/java/com/xiaojinzi/component2/view/Test1Fragment.kt
@@ -1,9 +1,22 @@
 package com.xiaojinzi.component2.view
 
+import android.os.Bundle
 import android.support.v4.app.Fragment
 
 import com.xiaojinzi.component.anno.FragmentAnno
 
-@FragmentAnno(value = ["component2.test1Fragment"])
-class Test1Fragment : Fragment()
+@FragmentAnno(value = ["component2.test1Fragment1"])
+class Test1Fragment : Fragment() {
+
+    companion object {
+        @FragmentAnno("component2.test1Fragment2")
+        @JvmStatic
+        fun newInstance(bundle: Bundle): Test1Fragment {
+            val f = Test1Fragment()
+            f.arguments = bundle
+            return f
+        }
+    }
+
+}
 
