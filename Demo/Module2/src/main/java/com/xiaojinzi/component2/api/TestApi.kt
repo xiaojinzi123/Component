050672714ae9b107705126d419949d596993ae82
xiaojinzi <cxj0xiaojinzi@gmail.com>
2021-06-22 22:49:26 +0800

    feat: 优化生成的代码的显示

--- Demo/Module2/src/main/java/com/xiaojinzi/component2/api/TestApi.kt
+++ Demo/Module2/src/main/java/com/xiaojinzi/component2/api/TestApi.kt
@@ -1,18 +1,23 @@
 package com.xiaojinzi.component2.api
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
-import com.xiaojinzi.component.anno.router.HostAndPathAnno
-import com.xiaojinzi.component.anno.router.NavigateAnno
-import com.xiaojinzi.component.anno.router.RouterApiAnno
+import com.xiaojinzi.base.interceptor.TimeConsumingInterceptor
+import com.xiaojinzi.component.anno.ParameterAnno
+import com.xiaojinzi.component.anno.router.*
 import com.xiaojinzi.component.bean.ActivityResult
 import com.xiaojinzi.component.impl.Call
+import io.reactivex.Completable
 
 @RouterApiAnno
 interface TestApi {
 
-    @HostAndPathAnno("test/test")
-    suspend fun go()
+    @HostAndPathAnno("user/detail")
+    fun go(
+            context: Context,
+            @ParameterAnno("userId") userId: String
+    ): Completable
 
     @NavigateAnno(forResult = true)
     @HostAndPathAnno("test/test")
@@ -37,5 +42,10 @@ interface TestApi {
     @NavigateAnno(resultCodeMatch = Activity.RESULT_CANCELED)
     @HostAndPathAnno("test/test")
     suspend fun go6()
+
+    @NavigateAnno(resultCodeMatch = Activity.RESULT_CANCELED)
+    @HostAndPathAnno("test/test")
+    @UseInteceptorAnno(classes = [TimeConsumingInterceptor::class])
+    suspend fun go7()
 
 }
