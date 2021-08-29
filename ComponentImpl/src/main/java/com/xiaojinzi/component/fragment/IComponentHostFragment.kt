0c881c496c64f03038f301ea1c3b640119855418
xiaojinzi <cxj0xiaojinzi@gmail.com>
2021-08-29 18:38:19 +0800

    feat: 转化类为 Kotlin

--- ComponentImpl/src/main/java/com/xiaojinzi/component/fragment/IComponentHostFragment.kt
+++ ComponentImpl/src/main/java/com/xiaojinzi/component/fragment/IComponentHostFragment.kt
@@ -1,18 +1,13 @@
-package com.xiaojinzi.component.fragment;
+package com.xiaojinzi.component.fragment
 
-
-import androidx.annotation.NonNull;
-import androidx.annotation.UiThread;
-
-import com.xiaojinzi.component.support.IBaseLifecycle;
-import com.xiaojinzi.component.support.IHost;
-
-import java.util.Set;
+import androidx.annotation.UiThread
+import com.xiaojinzi.component.support.IBaseLifecycle
+import com.xiaojinzi.component.support.IHost
 
 /**
  * 每个模块的接口,需要有生命周期的通知
  */
-public interface IComponentHostFragment extends IBaseLifecycle, IHost {
+interface IComponentHostFragment : IBaseLifecycle, IHost {
 
     /**
      * 主要用于检查模块和模块之间的重复
@@ -20,8 +15,7 @@ public interface IComponentHostFragment extends IBaseLifecycle, IHost {
      * 第一：因为这是一个 Map, 有重复的也会被覆盖
      * 第二：在注解驱动器中, 做了避免重复的操作
      */
-    @NonNull
-    @UiThread
-    Set<String> getFragmentNameSet();
+    @get:UiThread
+    val fragmentNameSet: Set<String>
 
 }
